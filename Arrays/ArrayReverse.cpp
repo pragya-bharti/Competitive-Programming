5ef00d63229eea17a79bbaa252e46b01b5e9c20f
Gyanesh Samanta <mail.gyaneshsamanta@gmail.com>
2021-06-23 15:32:10 +0530

    Added Array Programs

---
+++ Arrays/ArrayReverse.cpp
@@ -0,0 +1,22 @@
+// This program displays the array in reverse order without actually storing the reversed values in the array.
+
+#include<iostream>
+using namespace std;
+int main()
+{
+  int size=0,i=0;
+  cout<<"Enter size of the Array ";
+  cin>>size;
+  int array[size];
+  cout<<"Input values in the Array\n";
+  for(i=0;i<size;i++)
+   {
+     cin>>array[i];
+   }
+  cout<<"The reversed Array is\n";
+  for(i=size-1;i>=0;i--) // i starts from the last element and iterates to the first
+   {
+     cout<<array[i]<<"\n";
+   }
+  return 0;
+}
---
+++ Arrays/ArrayReverse2.cpp
@@ -0,0 +1,29 @@
+// This program using a second array to store the reversed values
+
+#include<iostream>
+using namespace std;
+int main()
+{
+  int size;
+  cout<<"Enter size of the Array\n";
+  cin>>size;
+  int array[size], revarray[size];
+  int i=0,j=size;
+  cout<<"Enter the array to be reversed: \n";
+  for(i=0;  i<size; i++) //simultaneously stores in proper order in array and in reverse order in revarray
+   {
+     cin>>array[i];
+     revarray[(size-1)-i]=array[i];
+   }
+  cout<<"The entered array is: \n";
+  for(i=0;i<size;i++)
+  {
+    cout<<array[i]<<"\n";
+  }
+  cout<<"The reversed array is: \n";
+  for(i=0;i<size;i++)
+  {
+    cout<<revarray[i]<<"\n";
+  }
+  return 0;
+}
